a476d0ba34b255ae6d8b662021e695751b2c3907
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2022-11-07 16:47:21 +0100

    style and js competed

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-export async function getAdvice() {
-  //get advices from https://api.adviceslip.com/advice
-  try {
-    const response = await fetch("https://api.adviceslip.com/advice");
-    const advices = await response.json();
-    return advices;
-  } catch (error) {
+const adviceButton = document.querySelector(".advice-btn");
+const adviceId = document.getElementById("advice_id");
+const quote = document.querySelector(".card-text");
+
+//get random advices from https://api.adviceslip.com/advice
+
+const getAdvice = async () => {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  if (response) {
+    const data = await response.json();
+    adviceId.innerText = data.slip.id;
+    quote.innerHTML = `<p class="quote">“${data.slip.advice}”</p>`;
+  } else {
     console.log(error);
   }
-}
+};
+
+//Function to display random advice when user click on button
+
+adviceButton.addEventListener("click", getAdvice);
+getAdvice();
